546111e756b2d2f7c9dc885ea08633ad005c3f6f
Max <maximumsomething@gmail.com>
2019-02-06 14:26:01 -0500

    Brain fart 2

--- start_streaming.sh
+++ start_streaming.sh
@@ -3,5 +3,5 @@
 if [ -z "$PI_ADDR" ]; then PI_ADDR=raspberrypi.local; fi
 if [ -z "$GST_COMMAND" ]; then GST_COMMAND=gst-launch-1.0; fi
 
-echo "" | nc $PI_ADDR 8082 && \
+echo "" | nc $PI_ADDR 8080 && \
 $GST_COMMAND udpsrc port=1234 ! gdpdepay ! rtph264depay ! avdec_h264 ! autovideosink sync=false
